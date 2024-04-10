2cd00f20f06e10c9062877328ea76f92f78f21c4
Andre_sn <salvadorclesmer@gmail.com>
2024-04-10 15:35:08 -0600

    cambios

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,43 @@
+
+
 import 'package:flutter/material.dart';
-import './src/app.dart'; 
-void main() {
-  runApp(const MyApp());
+
+void main(){ 
+  runApp(const MyApp()); 
+}
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Imagenes",
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Imagenes del Universo"),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              
+              const Text('Imagenes de James webb'),
+              SizedBox(
+                height: 250.0,
+                width: 350.0,
+                child: Image.asset("./assets/webb1.webp", fit: BoxFit.fitWidth,),
+              ), 
+              SizedBox(
+                width: 350.0,
+                height: 350.0,
+                child: Image.network("https://cdn.forbes.com.mx/2023/08/Nebulosa-Anillo-James-Webb.webp", fit: BoxFit.fitWidth,),
+              ) 
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
